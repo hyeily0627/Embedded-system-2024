1d75a006cb8058089cc977aaee992ca6fb3909fc
OH HYE JIN <156732476+hyeily0627@users.noreply.github.com>
2024-03-20 10:46:26 +0900

    Create
    함수로 구현

---
+++ day08/sort01_02.c
@@ -0,0 +1,28 @@
+//함수로 구현하기
+
+#include <stdio.h>
+
+void arysort(int *ary, int size) {
+    int i, j, temp = 0;
+    for (i = 0; i < size -1; i++) {
+        for (j = 0; j < size -1; j++) {
+            if (ary[j] > ary[j + 1]) {
+                temp = ary[j];
+                ary[j] = ary[j + 1];
+                ary[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int ary[] = {9, 4, 15};
+    int size = sizeof(ary) / sizeof(ary[0]);     //전체/방한개 = (8*4byte)/(1*4byt>
+
+    arysort(ary, size);
+
+    printf("오름차순으로 정렬된 배열: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", ary[i]);
+    }
+}
