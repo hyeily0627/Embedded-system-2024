b35e5292b245c9b4602288682a229631ee4186d8
OH HYE JIN <156732476+hyeily0627@users.noreply.github.com>
2024-03-21 10:15:01 +0900

    Update

--- day08/file06.c
+++ day08/file06.c
@@ -1,34 +1,31 @@
-뭘해야하는지 정말 모르겠당
-1 #include <stdio.h>
- 2 #include <stdlib.h>
- 3 #include <fcntl.h>
- 4 #include <unistd.h>
- 5 void error_handling(char* message);
- 6
+ 1  /*
+ 2  파일 이름 : 이름.txt, data: 정보
+ 3  복사할 파일 이름: 이름copy.txt
+ 4  */
+ 5
+ 6  #include <stdio.h>
  7
- 8 int main()
- 9 {
-10    char name[20] = "오혜진\n";
-11    char job[20] = "Student\n";
-12    int age = 27;
-13
-14    fp = open("hyejin.txt", O_CREAT | O_WRONLY);
-15
-16    if(fp==-1)
-17      error_handling("open() error!");
-18      printf("file descriptor : %d \n", fp);
-19
-20    if(write(fp, buf, sizeof(buf))==-1)
-21      error_handling("open() error!");
-22      close(fp);
-23      return 0;
-24  }
-25
-26  void error_handling(char* message)
-27  {
-28    fputs(message, stderr);
-29    fputc('\n',stderr);
-30    exit(1);
-31 }
-32
+ 8  void main()
+ 9  {
+10    char myInfo[30] = "ohhyejin,1014372";
+11    char myInfoCopy[30];
+12
+13    //파일생성
+14    FILE* origen;
+15    origen = fopen("오혜진.txt", "w");
+16    fprintf(origen, "%s", myInfo);
+17
+18
+19    // 원본파일 데이터 읽기
+20    origen = fopen("오혜진.txt", "r");
+21    fscanf(origen, "%s", myInfoCopy);
+22    fclose(origen);
+23
+24    FILE* copy;
+25    copy = fopen("오혜진copy.txt", "w");
+26    fprintf(copy, "%s", myInfoCopy);
+27
+28    fclose(copy);
+29  }
+30
 
